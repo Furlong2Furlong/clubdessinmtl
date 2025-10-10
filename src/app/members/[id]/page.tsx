1d601ee17f813a8f281d3230f9b847b46a784fc2
furlong2furlong <furlongmichelle@gmail.com>
2025-10-09 21:29:03 -0400

    memebers

--- src/app/members/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Members() {
-    return(
-        <div className="text-center">Members</div>
-        // Array of members from database 
-    )
-}
---
+++ src/app/members/page.tsx
@@ -0,0 +1,5 @@
+export default function Members() {
+    return(
+        <div className="text-center">Members</div>
+    )
+}
